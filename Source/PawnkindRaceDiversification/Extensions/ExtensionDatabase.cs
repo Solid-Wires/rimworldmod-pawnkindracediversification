3f9d813db88d50274b136b0b7a5853b08ef00fef
Solid Wires <solid.wires@gmail.com>
2021-04-18 16:13:45 -0400

    Pawnmorpher compatibility added

--- Source/PawnkindRaceDiversification/Extensions/ExtensionDatabase.cs
+++ Source/PawnkindRaceDiversification/Extensions/ExtensionDatabase.cs
@@ -8,6 +8,7 @@ namespace PawnkindRaceDiversification.Extensions
     {
         internal static Dictionary<string, RaceDiversificationPool> racesDiversified = new Dictionary<string, RaceDiversificationPool>();
         internal static Dictionary<string, ThingDef_AlienRace> racesLoaded = new Dictionary<string, ThingDef_AlienRace>();
+        internal static List<string> impliedRacesLoaded = new List<string>();
         internal static List<PawnKindDef> pawnKindDefsExcluded = new List<PawnKindDef>();
         internal static Dictionary<PawnKindDef, string> pawnKindRaceDefRelations = new Dictionary<PawnKindDef, string>();
     }
