c96d7bbd366ed5e8eb41c18846d6a90d264062fe
Solid Wires <solid.wires@gmail.com>
2021-07-19 12:26:38 -0400

    Cleaning up source folder

---
+++ Source/Patches/AnyModGeneratedPawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace PawnkindRaceDiversification.Patches
+{
+    public static class AnyModGeneratedPawn
+    {
+        //Harmony manual prefix method
+        public static void OnModGeneratedPawn()
+        {
+            PawnkindGenerationHijacker.PauseWeightGeneration();
+        }
+    }
+}
